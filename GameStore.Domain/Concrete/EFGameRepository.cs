b10e09182a2966f4bf556b19ca36fc3bd04346fc
Kagel1605 <48802268+Kagel1605@users.noreply.github.com>
2021-03-14 16:39:33 +0400

    DB relation included

---
+++ GameStore.Domain/Concrete/EFGameRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+
+namespace GameStore.Domain.Concrete
+{
+    public class EFGameRepository:IGameRepository
+    {
+        EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Game> Games => context.Games;
+    }
+}
--- GameStore.WebUI/Infrastructure/NinjectDependenctResolver.cs
+++ GameStore.WebUI/Infrastructure/NinjectDependenctResolver.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using GameStore.Domain.Entities;
 using GameStore.Domain.Abstract;
-using Moq;
+using GameStore.Domain.Concrete;
 using Ninject;
 
 namespace GameStore.WebUI.Infrastructure
@@ -32,14 +29,7 @@ namespace GameStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            Mock<IGameRepository> mock = new Mock<IGameRepository>();
-            mock.Setup(m => m.Games).Returns(new List<Game>
-            {
-                new Game { Name = "SimCity", Price = 1499 },
-                new Game { Name = "TITANFALL", Price=2299 },
-                new Game { Name = "Battlefield 4", Price=899.4M }
-            });
-            kernel.Bind<IGameRepository>().ToConstant(mock.Object);
+            kernel.Bind<IGameRepository>().To<EFGameRepository>();
         }
     }
 }
